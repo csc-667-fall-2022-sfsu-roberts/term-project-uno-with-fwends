b72840efd9742646081a8cd3d91a46556d5e1065
itachi <jlopex83@gmail.com>
2022-11-25 17:53:39 -0800

    24c:jq Socket io initialization with server and injected into app object

---
+++ sockets/initialize.js
@@ -0,0 +1,31 @@
+const socketsIO = require("socket.io");
+const sessionMiddleware = require("../app-config/sessions")
+
+const initialize = (httpServer, app) => {
+    const io = new socketsIO.Server(httpServer); 
+
+    const wrap = (middleware) => (socket, next) =>
+        middleware(socket.request, {}, next);
+
+    io.use(wrap(sessionMiddleware));
+
+    io.use((socket, next) => {
+        const session = socket.request.session;
+
+        if(session !== undefined && session.authenticated == true){
+            next(); 
+        } else {
+            next(new Error("Unauthorized"));
+        }
+    });
+
+    io.on("connection", (socket) => {
+        console.log({
+            message: "Connection happened",
+            session_id: socket.request.session, 
+        }); 
+    });
+    app.io = io; 
+};
+
+module.exports = initialize; 
