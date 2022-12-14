3c75d599d16a254c8babcf97e55307ab09f4346a
itachi <jlopex83@gmail.com>
2022-12-13 23:45:21 -0800

    29c:jq added logic to add user to game, and refactored games db sql...

--- routes/authenticated/games.js
+++ routes/authenticated/games.js
@@ -14,8 +14,22 @@ router.post("/create", (request, response) => {
     .catch((error) => {
       console.log(error);
       response.status(500).send(); 
+    });
+}); 
+
+router.post("/:id/join", (request, response) => {
+  const {id: game_id} = request.params; 
+  const { user_id } = request.session;
+
+  Games.join(game_id, user_id)
+    .then(() => {
+      response.redirect(`/games/${game_id}`); 
     })
-})
+    .catch((error) => {
+      console.log(error);
+      response.status(500).send(); 
+    });
+}); 
 
 /* GET home page. */
 router.get("/:id", (request, response) => {
