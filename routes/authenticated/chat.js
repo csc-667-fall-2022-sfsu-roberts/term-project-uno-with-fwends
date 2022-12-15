5bfa77fde3187e580cf0495d397235f112eeb2e0
itachi <jlopex83@gmail.com>
2022-12-15 10:24:54 -0800

    32c:jq pushing branch jose to main...

--- routes/authenticated/chat.js
+++ routes/authenticated/chat.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); 
 
 /* GET home page. */
 router.post("/:id", (request, response) => {
@@ -13,4 +13,27 @@ router.post("/:id", (request, response) => {
   response.sendStatus(200); 
 });
 
-module.exports = router; 
+module.exports = router;  
+
+
+/* const express = require('express');
+const router = express.Router();
+
+router.post('/:id', (req, res, next) =>{
+  const {id} = req.params;
+  const {message} = req.body;
+  const {username} = req.session;
+  const timestamp = Date.now();
+
+  req.app.io.emit(`chat:${id}`, {
+    sender: username,
+    message,
+    timestamp,
+  })
+
+  res.sendStatus(200);
+
+})
+
+
+module.exports = router;  */
