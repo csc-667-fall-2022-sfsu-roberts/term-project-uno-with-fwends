c4760a123eec0797cd890d56ecf1d1a2a649b5b8
EJ Cenario <kurtina09@yahoo.com>
2022-12-14 21:20:47 -0800

    adding starter files, getting ready to build

---
+++ config/cards.js
@@ -0,0 +1,39 @@
+const DRAW_TWO = 10;
+const DRAW_FOUR = 11;
+const WILD = 12;
+const REVERSE = 13;
+const SKIP = 14;
+
+const SINGLE_COLOR_CARD_TYPES = [0];
+
+const DUAL_COLOR_CARD_TYPES = [
+	1,
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9,
+	DRAW_TWO,
+	REVERSE,
+	SKIP,
+];
+
+const NO_COLOR_CARD_TYPES = [DRAW_FOUR, WILD];
+
+const BLUE = 'blue';
+const YELLOW = 'yellow';
+const RED = 'red';
+const GREEN = 'green';
+const NONE = 'none';
+
+const CARD_COLORS = { BLUE, YELLOW, RED, GREEN, NONE };
+
+module.exports = {
+	SINGLE_COLOR_CARD_TYPES,
+	DUAL_COLOR_CARD_TYPES,
+	NO_COLOR_CARD_TYPES,
+	CARD_COLORS,
+};
